d4c3e70df394e6bf35914366b362b721b4d2fbac
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-09-22 00:39:56 +0000

    removed spinner1

--- spinner2.js
+++ spinner2.js
@@ -1,39 +1,3 @@
-// setTimeout(()=> {
-//   process.stdout.write('\r|    ');
-// }, 100);
-
-// setTimeout(() => {
-//   process.stdout.write('\r/   ');
-// }, 300);
-
-// setTimeout(() => {
-//   process.stdout.write('\r-   ');
-// }, 500);
-
-// setTimeout(() => {
-//   process.stdout.write('\r\\   ');
-// }, 700);
-
-// setTimeout(()=> {
-//   process.stdout.write('\r|    ');
-// }, 800);
-
-// setTimeout(()=> {
-//   process.stdout.write('\r\\   ');
-// }, 900);
-
-// setTimeout(()=> {
-//   process.stdout.write('\r-   ');
-// }, 1000);
-
-// setTimeout(()=> {
-//   process.stdout.write('\r/   ');
-// }, 1100);
-
-// setTimeout(()=> {
-//   process.stdout.write('\r|    ');
-// }, 1200);
-
 //create an array to hold all the symbols
 const symbol = ["|", "-", "\\", "|", "-", "\\", "|"];
 //variable declaration
